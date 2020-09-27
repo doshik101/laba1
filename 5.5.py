64a524922b6e17604652c7a3b5fb4fe4d7abff9a
doshik101 <71586911+doshik101@users.noreply.github.com>
2020-09-27 12:22:50 +0300

    Create 5.5.py

---
+++ 5.5.py
@@ -0,0 +1,10 @@
+c=int(input("Введите код города: "))
+t=float(input("Введите длительность разговора в минутах: "))
+if(c==343):
+    print(15*t)
+elif(c==381):
+    print(18*t)
+elif(c==473):
+    print(13*t)
+elif(c==485):
+    print(11*t)
